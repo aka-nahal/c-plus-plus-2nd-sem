8cdf96a2c8dffe6a89939a26c4f96f3289b2a24f
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2023-02-14 06:48:12 +0000

    average-marks

---
+++ average-marks.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c,d,e;
+
+    cout<<"Enter your marks accordindly :-  "<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+
+    cout<<"Average :- "<<(a+b+c+d+e)/5<<endl;
+
+}
