72e4714e88696d9c484e483bfbcc04d934fd2f0d
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2023-02-14 06:32:49 +0000

    program to get uid

---
+++ get-uids.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name;
+
+    int age;
+
+    string uid;
+
+    cout<<"Enter your name, uid and age respectively"<<endl;
+
+cin>>name;
+cin>>age;
+cin>>uid;
+
+cout<<"UID:- "<<uid<<endl<<"Age:- "<<age<<endl<<"Name:- "<<name<<endl;
+
+
+
+    return 0;
+}
