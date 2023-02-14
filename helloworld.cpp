eac4614d864de800de0a89e002431851623ba711
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2023-02-14 06:11:08 +0000

    new

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
-using namespace std;
+#include<iostream> // header file
+using namespace std; 
 
 int main(){
 
-    cout<<"Hello World"<<endl;
+    cout<<"Hello World"<<endl; // "<<" instertion operation
 
     int a;
-    cin>>a;
+    cin>>a; // ">>" extraction operator
 
     cout<<a<<endl;;
 
